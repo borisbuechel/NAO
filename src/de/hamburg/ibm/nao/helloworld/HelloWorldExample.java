fcd9220f34663de5653e5d691f0e5523bb91602c
borisbuechel <boris.buechel@de.ibm.com>
2016-10-07 16:12:29 +0200

    First productive code

--- src/de/hamburg/ibm/nao/helloworld/HelloWorldExample.java
+++ src/de/hamburg/ibm/nao/helloworld/HelloWorldExample.java
@@ -3,8 +3,7 @@ package de.hamburg.ibm.nao.helloworld;
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
 	}
 
 }
